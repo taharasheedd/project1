e2e68bad4c8a7a6c863cab6b159980ff42ed5e10
taharasheedd <taharasheedd@users.noreply.github.com>
2020-12-24 18:17:59 +0500

    s

---
+++ expensetracker/src/components/AddTransaction.js
@@ -0,0 +1,42 @@
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Transaction Details</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            <u>Amount</u> <br/>
+            To Register an Income Enter a Positive Value<br/>
+             To Register an Expense enter a Negaive Value
+            </label>
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
